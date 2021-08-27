6870308c4c261e56a605c8ebec2687516d0abc9c
promehedi <mmhasan563@gmail.com>
2021-08-27 13:36:33 +0600

    Convert to Styled Component

---
+++ src/features/restaurants/components/RestaurantCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Card } from 'react-native-paper'
+import styled from 'styled-components'
+
+const Root = styled(View)``
+
+const AppCard = styled(Card)``
+
+const CardCover = styled(Card.Cover)``
+
+const CardContent = styled(Card.Content)`
+  padding: 15px;
+`
+
+const Title = styled(Text)`
+  font-size: 20px;
+  font-weight: bold;
+`
+const Paragraph = styled(Text)`
+  font-size: 16px;
+`
+
+const RestaurantCard = ({ restaurant = {} }) => {
+  const {
+    name = 'Some Restaurant',
+    icon = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    photos = ['https://picsum.photos/400/370'],
+    address = '100 some random street',
+    isOpenNow = true,
+    rating = 4.5,
+    isClosedTemporarily = false,
+    phone = '+1 (123) 456-7890',
+    website = 'https://www.google.com',
+    price = '$$'
+  } = restaurant
+
+  return (
+    <Root>
+      <AppCard>
+        <CardCover source={{ uri: photos[0] }} />
+        <CardContent>
+          <Title>{name}</Title>
+          <Paragraph>Card content</Paragraph>
+        </CardContent>
+      </AppCard>
+    </Root>
+  )
+}
+
+export default RestaurantCard
--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -1,42 +1,43 @@
 import React, { useState } from 'react'
-import { SafeAreaView, StatusBar, StyleSheet, View } from 'react-native'
+import { SafeAreaView, StatusBar, View } from 'react-native'
 import { Searchbar } from 'react-native-paper'
-import RestaurantInfo from '../components/RestaurantInfo'
+import styled from 'styled-components'
+import RestaurantInfo from '../components/RestaurantCard'
+
+const Root = styled(SafeAreaView)`
+  flex: 1;
+  background-color: #fff;
+  margin-top: ${StatusBar.currentHeight || 0}px;
+`
+
+const SearchWrap = styled(View)`
+  padding: 15px;
+  background-color: #fff;
+`
+
+const Content = styled(View)`
+  flex: 1;
+  padding: 15px;
+  background-color: #f0f0f0;
+`
 
 const RestaurantsScreen = () => {
   const [query, setQuery] = useState('')
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.searchBar}>
+    <Root>
+      <SearchWrap>
         <Searchbar
           placeholder="Search Here"
           value={query}
           onChangeText={(text) => setQuery(text)}
         />
-      </View>
-      <View style={styles.content}>
+      </SearchWrap>
+      <Content>
         <RestaurantInfo />
-      </View>
-    </SafeAreaView>
+      </Content>
+    </Root>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: StatusBar.currentHeight || 0
-  },
-  searchBar: {
-    padding: 15,
-    backgroundColor: '#fff'
-  },
-  content: {
-    flex: 1,
-    padding: 15,
-    backgroundColor: '#f0f0f0'
-  }
-})
 
 export default RestaurantsScreen
