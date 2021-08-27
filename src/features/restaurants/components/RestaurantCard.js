5294ac4b1ae166f4022bf41560921b4d1cb85b58
promehedi <mmhasan563@gmail.com>
2021-08-27 14:41:46 +0600

    Add Ratings

--- src/features/restaurants/components/RestaurantCard.js
+++ src/features/restaurants/components/RestaurantCard.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import { Card } from 'react-native-paper'
 import styled from 'styled-components'
+import { SvgXml } from 'react-native-svg'
+import star from '../../../../assets/star'
 
 const Root = styled(View)``
 
@@ -13,13 +15,26 @@ const CardContent = styled(Card.Content)`
   padding: ${(props) => props.theme.space[3]};
 `
 
+const Rating = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`
+
+const RateCount = styled(Text)`
+  font-size: ${(props) => props.theme.fontSizes.body};
+  margin-right: ${(props) => props.theme.space[1]};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+`
+
 const Title = styled(Text)`
   font-family: ${(props) => props.theme.fonts.heading};
   font-size: ${(props) => props.theme.fontSizes.title};
   font-weight: ${(props) => props.theme.fontWeights.bold};
   color: ${(props) => props.theme.colors.ui.primary};
 `
-const Paragraph = styled(Text)`
+const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.secondary};
@@ -39,13 +54,21 @@ const RestaurantCard = ({ restaurant = {} }) => {
     price = '$$'
   } = restaurant
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)))
+
   return (
     <Root>
       <AppCard>
         <CardCover source={{ uri: photos[0] }} />
         <CardContent>
           <Title>{name}</Title>
-          <Paragraph>Card content</Paragraph>
+          <Rating>
+            <RateCount>{rating}</RateCount>
+            {ratingArray.map((_, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <Address>Card content</Address>
         </CardContent>
       </AppCard>
     </Root>
