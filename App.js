32fdb273ea47ea33fff91a9816caeaf783e22373
promehedi <mmhasan563@gmail.com>
2021-08-28 04:28:55 +0600

    Add BottomTabs

--- App.js
+++ App.js
@@ -7,10 +7,24 @@ import {
   Oswald_400Regular
 } from '@expo-google-fonts/oswald'
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
+import { Ionicons } from '@expo/vector-icons'
 
 import RestaurantsScreen from './src/features/restaurants/screens/RestaurantsScreen'
 import { ThemeProvider } from 'styled-components/native'
 import { theme } from './src/infrastructure/theme/index'
+import SafeArea from './src/utils/SafeArea'
+import { Text } from 'react-native'
+
+const Maps = () => (
+  <SafeArea>
+    <Text>Maps Screen</Text>
+  </SafeArea>
+)
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings Screen</Text>
+  </SafeArea>
+)
 
 const App = () => {
   const [oswaldLoaded] = useOswald({
@@ -30,8 +44,42 @@ const App = () => {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Navigator
+            tabBarOptions={{
+              activeTintColor: '#e91e63',
+              inactiveTintColor: 'gray'
+            }}
+          >
+            <Tab.Screen
+              name="Restaurants"
+              component={RestaurantsScreen}
+              options={{
+                tabBarLabel: 'Restaurants',
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name="md-restaurant" color={color} size={26} />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="Maps"
+              component={Maps}
+              options={{
+                tabBarLabel: 'Maps',
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name="md-map" color={color} size={26} />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={Settings}
+              options={{
+                tabBarLabel: 'Settings',
+                tabBarIcon: ({ color }) => (
+                  <Ionicons name="md-settings" color={color} size={26} />
+                )
+              }}
+            />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
