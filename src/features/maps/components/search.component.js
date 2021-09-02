24368d1036a85a00a416aa2e4853ccd603c1bfa4
promehedi <mmhasan563@gmail.com>
2021-09-02 15:48:34 +0600

    Map Screen & Search Bar

--- src/features/maps/components/search.component.js
+++ src/features/maps/components/search.component.js
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { View } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import styled from 'styled-components/native'
 import { Searchbar } from 'react-native-paper'
 import { LocationContext } from '../../../services/location/location.context'
 
 export const Root = styled(View)`
+  position: absolute;
+  width: 100%;
   padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
+  z-index: 9;
+  top: ${StatusBar.currentHeight || 0}px;
 `
 
 const Search = () => {
@@ -21,6 +24,7 @@ const Search = () => {
     <Root>
       <Searchbar
         placeholder="Search location (Chicago)"
+        icon="map"
         value={query}
         onChangeText={(text) => setQuery(text)}
         onSubmitEditing={() => search(query)}
--- src/features/maps/screens/map.screen.js
+++ src/features/maps/screens/map.screen.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import MapView from 'react-native-maps'
+import styled from 'styled-components/native'
+import Search from '../components/search.component'
+
+const Map = styled(MapView)`
+  width: 100%;
+  height: 100%;
+`
 
 const MapScreen = () => {
   return (
     <>
-      <MapView style={{ height: '100%' }} />
+      <Search />
+      <Map />
     </>
   )
 }
