2d1b3ae21e59953573262658de1e3834dd574037
promehedi <mmhasan563@gmail.com>
2021-09-02 16:29:30 +0600

    Setting the Map Region

--- src/features/maps/screens/map.screen.js
+++ src/features/maps/screens/map.screen.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import MapView from 'react-native-maps'
 import styled from 'styled-components/native'
+
+import { LocationContext } from '../../../services/location/location.context'
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
 import Search from '../components/search.component'
 
 const Map = styled(MapView)`
@@ -9,10 +12,46 @@ const Map = styled(MapView)`
 `
 
 const MapScreen = () => {
+  const [latDelta, setLatDelta] = useState('')
+
+  const { location } = useContext(LocationContext)
+  const { restaurants = [] } = useContext(RestaurantsContext)
+
+  const { lat, lng, viewport } = location
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat
+    const southwestLat = viewport.southwest.lat
+
+    setLatDelta(northeastLat - southwestLat)
+  }, [viewport])
+
   return (
     <>
       <Search />
-      <Map />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: Number(latDelta),
+          longitudeDelta: 0.02
+        }}
+      >
+        {restaurants.map((restaurant, i) => {
+          const { lat: latCoordinate, lng: lngCoordinate } =
+            restaurant.geometry.location
+          return (
+            <MapView.Marker
+              key={i}
+              coordinate={{
+                latitude: latCoordinate,
+                longitude: lngCoordinate
+              }}
+              title="Marker"
+            />
+          )
+        })}
+      </Map>
     </>
   )
 }
